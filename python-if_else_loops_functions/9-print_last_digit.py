72e8da918044a575cc3982ec679fbdfa36071818
CashlockH <ilyashesenli48@gmail.com>
2024-05-19 18:29:52 +0400

    output correction

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -4,7 +4,5 @@ def print_last_digit(number):
     if temp < 0:
         temp = temp * - 1
     last = temp % 10
-    if number < 0:
-        last = temp % 10 * -1
     print(last, end="")
     return last
