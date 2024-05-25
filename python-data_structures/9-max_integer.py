b3606be86183eed20df2178e80306bf6a174d783
CashlockH <ilyashesenli48@gmail.com>
2024-05-26 03:18:09 +0400

    pycodestyle

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     max_value = my_list[0]
-    for i,j in enumerate(my_list):
+    for i, j in enumerate(my_list):
         if j > max_value:
             max_value = j
     return max_value
