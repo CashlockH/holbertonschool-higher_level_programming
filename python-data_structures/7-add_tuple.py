256d9f21c53dc3325d6e2b1a6313705031c49075
CashlockH <ilyashesenli48@gmail.com>
2024-05-26 02:49:15 +0400

    condition is corrected

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -7,7 +7,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_a) >= 2:
         for i in range(0,2):
             ah[i] = tuple_a[i]
-    if len(tuple_a) == 1:
+    if len(tuple_b) == 1:
         bh[0] = tuple_b[0]
     elif len(tuple_b) >= 2:
         for i in range(0,2):
