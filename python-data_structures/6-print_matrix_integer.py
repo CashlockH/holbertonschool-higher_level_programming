f6d50a568226fd127e08a234dafd023dc7236e83
CashlockH <ilyashesenli48@gmail.com>
2024-05-26 01:33:44 +0400

    format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for i, j in enumerate(matrix):
         for a, b in enumerate(j):
             if a != len(j) - 1:
-                print(b, end = " ")
+                print("{:d}".format(b), end = " ")
             else:
-                print(b)
+                print("{:d}".format(b))
