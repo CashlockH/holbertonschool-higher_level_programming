bbe669ac0bced36771cf0065f675c6adba0784fc
CashlockH <ilyashesenli48@gmail.com>
2024-05-26 02:46:32 +0400

    first 2 element

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -5,12 +5,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) == 1:
         ah[0] = tuple_a[0]
     elif len(tuple_a) >= 2:
-        for i,j in enumerate(tuple_a):
+        for i in range(0,2):
             ah[i] = tuple_a[i]
     if len(tuple_a) == 1:
         bh[0] = tuple_b[0]
     elif len(tuple_b) >= 2:
-        for i,j in enumerate(tuple_b):
+        for i in range(0,2):
             bh[i] = tuple_b[i]
     new_list = (ah[0] + bh[0], ah[1] + bh[1])
     return new_list
