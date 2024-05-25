cc8ae6422dbe98650cdb4b1e289def785f5a6e71
CashlockH <ilyashesenli48@gmail.com>
2024-05-26 01:45:12 +0400

    empty matrix condition

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i, j in enumerate(matrix):
-        for a, b in enumerate(j):
-            if a != len(j) - 1:
-                print("{:d}".format(b), end = " ")
-            else:
-                print("{:d}".format(b))
+        if matrix == [[]]:
+            print()
+        else:
+            for a, b in enumerate(j):
+                if a != len(j) - 1:
+                    print("{:d}".format(b), end = " ")
+                else:
+                    print("{:d}".format(b))
