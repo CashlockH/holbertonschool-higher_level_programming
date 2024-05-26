d8e513c599c0693132d80c68d781fc16e6af4853
CashlockH <ilyashesenli48@gmail.com>
2024-05-26 17:48:41 +0400

    condition is added

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
+        if not my_list:
+            return 0
         for i, j in enumerate(my_list):
             if i < x:
                 print("{}".format(j), end="")
