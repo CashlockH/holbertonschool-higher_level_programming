95b82482e99f6c63e31dcab9b301b63cf7b700cf
CashlockH <ilyashesenli48@gmail.com>
2024-05-26 16:12:14 +0400

    condition added

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,10 +6,15 @@ def roman_to_int(roman_string):
     new = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
     for i in roman_string:
-        if i == 'I':
+        if i == 'X' and reducer == 0:
+            reducer = 10
+        elif i == 'I':
             reducer = 1
         elif (i == 'V' or i == 'X') and reducer == 1:
             total = total - 2
+            reducer = 0
+        elif i == "C" and reducer == 10:
+            total = total - 20
             reducer = 0
         else:
             reducer = 0
