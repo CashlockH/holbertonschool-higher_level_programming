1174112328ea104c17a3c417803d403b4f6d4999
CashlockH <ilyashesenli48@gmail.com>
2024-05-26 15:40:22 +0400

    map

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     a = 0
+    b = ""
     if not a_dictionary:
         return None
     for key, value in a_dictionary.items():
         if value >= a:
             a = value
-    return a
+            b = key
+    return b
