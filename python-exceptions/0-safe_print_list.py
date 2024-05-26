2c4c020ed09ee456db86aca3f3b435af24d2dd50
CashlockH <ilyashesenli48@gmail.com>
2024-05-26 17:56:57 +0400

    try print integer

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        if not my_list:
-            return 0
         for i, j in enumerate(my_list):
             if i < x:
                 print("{}".format(j), end="")
@@ -12,5 +10,5 @@ def safe_print_list(my_list=[], x=0):
         if i < x:
             return i + 1
         return i
-    except Exception as err:
-        print("this is not valid")
+    except IndexError:
+        return 0
