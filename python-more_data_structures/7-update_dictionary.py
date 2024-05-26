d0833f9a564a90d32f5cc5c6ed8a6d4e7bf2111f
CashlockH <ilyashesenli48@gmail.com>
2024-05-26 15:07:59 +0400

    updating dictionary

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+def update_dictionary(a_dictionary, key, value):
+    for keyo, vali in a_dictionary.items():
+        if keyo == key:
+            a_dictionary[keyo] = value
+            return a_dictionary
+    a_dictionary[keyo] = value
+    return a_dictionary
