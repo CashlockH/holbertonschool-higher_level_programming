5205801000132ce6cff0e61375227644dd895ef6
CashlockH <ilyashesenli48@gmail.com>
2024-05-26 14:49:29 +0400

    files changed

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def only_diff_elements(set_1, set_2):
-    new_set = set_1 ^ set_2
+def common_elements(set_1, set_2):
+    new_set = {x for x in set_1 if x in set_2}
     return new_set
--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    new_set = set(x for s in (set_1, set_2) for x in s)
+    new_set = set_1 ^ set_2
     return new_set
