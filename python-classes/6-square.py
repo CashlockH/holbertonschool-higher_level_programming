f3a3bdc4b30599e3d2d4d0f59ddee10f4bca89a9
CashlockH <ilyashesenli48@gmail.com>
2024-05-26 23:06:49 +0400

    position

---
+++ python-classes/6-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" This module holds Square class"""
+
+
+class Square:
+    """class with an private attribute and area method"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        for i in range(0, self.size):
+            print('#' * self.size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
