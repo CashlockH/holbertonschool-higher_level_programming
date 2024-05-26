7bcf44bcb287bf6f1ccd81f1901f2484f4cc0746
CashlockH <ilyashesenli48@gmail.com>
2024-05-26 16:06:24 +0400

    reducer & pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    a = 0
     if type(roman_string) is not str or not roman_string:
         return 0
+    reducer = 0
+    new = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
     for i in roman_string:
-            if i == 'X':
-                a = a + 10
-            elif i == 'I':
-                a = a + 1
-            elif i == 'V':
-                a = a + 5
-            elif i ==  'L':
-                a = a + 50
-            elif i == 'C':
-                a = a + 100
-            elif i == 'D':
-                a = a + 500
-            elif i == 'M':
-                a = a + 1000
-    return a
+        if i == 'I':
+            reducer = 1
+        elif (i == 'V' or i == 'X') and reducer == 1:
+            total = total - 2
+            reducer = 0
+        else:
+            reducer = 0
+        if i in new:
+            total = total + new[i]
+    return total
