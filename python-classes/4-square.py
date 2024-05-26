3642aa7df1c3fc3d89be44f00adffd4305130b1c
CashlockH <ilyashesenli48@gmail.com>
2024-05-26 22:32:38 +0400

    setter and getter

---
+++ python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" This module holds Square class"""
+
+
+class Square:
+    """class with an private attribute and area method"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.size = size
+
+    def area(self):
+        return self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        self.__size = value
