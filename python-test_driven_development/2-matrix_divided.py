7dfddf0874680d824c6af8fdf225a9ded69f7d34
CashlockH <ilyashesenli48@gmail.com>
2024-05-30 23:02:51 +0400

    test cases for matrix_division

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Division of list elementd"""
+
+
+def matrix_divided(a, b):
+    """Divide each element of list of list a by b"""
+
+    new_matrix = []
+    length = len(a[0])
+    if not isinstance(b, (int,float)):
+        raise TypeError("div must be a number")
+    elif b == 0:
+        raise ZeroDivisionError("division by zero")
+    for i in a:
+        if len(i) != length:
+            raise TypeError("Each row of the matrix must have the same size")
+        row = []
+        for j in i:
+            if isinstance(j, (float, int)):
+                row.append(round(j / b, 2))
+            else:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        new_matrix.append(row)
+        length = len(i)
+    return new_matrix
