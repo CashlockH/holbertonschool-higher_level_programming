8f78618c3273b42e89c12ce194a2ded6232d44ec
CashlockH <ilyashesenli48@gmail.com>
2024-05-31 00:12:30 +0400

    test cases for 3-say_my_name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def say_my_name(first_name, last_name=""):
+    if (c := isinstance(first_name, str)) and (d := isinstance(last_name, str)):
+        print("My name is {} {}".format(first_name, last_name))
+    elif not c:
+        raise TypeError("first_name must be a string")
+    elif not d:
+        raise TypeError("last_name must be a string")
