d2976b729957bbc75f177c256ea8dd3002cb8203
CashlockH <ilyashesenli48@gmail.com>
2024-05-31 00:14:41 +0400

    module documentation and pycodestyle

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+""" say_my_name module"""
+
+
 def say_my_name(first_name, last_name=""):
-    if (c := isinstance(first_name, str)) and (d := isinstance(last_name, str)):
+    """Prints first_name and last_name"""
+    if (c := isinstance(first_name, str)) and \
+            (d := isinstance(last_name, str)):
         print("My name is {} {}".format(first_name, last_name))
     elif not c:
         raise TypeError("first_name must be a string")
