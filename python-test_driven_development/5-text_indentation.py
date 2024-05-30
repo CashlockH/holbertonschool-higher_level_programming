2ace7a28f810cf3773170f7972be56a92a34a1d1
CashlockH <ilyashesenli48@gmail.com>
2024-05-31 02:03:32 +0400

    replace funciton added

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -4,19 +4,10 @@
 
 def text_indentation(text):
     """Tokenize and prints the text"""
-    a = 0
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+    text = text.replace(": ", ":").replace("? ", "?").replace(". ", ".")
     for i in text:
-        if i in (':', '.', '?'):
-            a = 1
-            print(i)
-        else:
-            if a == 1 and i == " ":
-                print()
-            elif a == 1 and i != " ":
-                print()
-                print(i, end="")
-            else:
-                print(i, end="")
-            a = 0
+        print(i, end="")
+        if i in (':', '?', '.'):
+            print('\n')
