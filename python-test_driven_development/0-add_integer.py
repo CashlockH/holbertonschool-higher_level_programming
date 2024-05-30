6451b703a2a69875086382fb97831083595a4b07
CashlockH <ilyashesenli48@gmail.com>
2024-05-30 11:43:10 +0000

    import is deleted

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-doctest_testfile.py
-import doctest
-if __name__ == '__main__':
-    doctest.testfile('add_integer.txt')
 def add_integer(a, b=98):
     if (c := isinstance(a, (float, int))) and (d := isinstance(b, (float, int))):
         if (c:= isinstance(a, float)) or (d:= isinstance(b, float)):
