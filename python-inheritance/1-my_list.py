dfa0fae4d881ce8eb2a352189bfdabe068b746b2
CashlockH <ilyashesenli48@gmail.com>
2024-06-01 23:26:17 +0400

    return correction

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,6 +5,6 @@
 class MyList(list):
     """inherit from list and sort the list"""
     def print_sorted(self):
-        new = sorted(self)
-        print(new)
-        return new
+        new = self.copy()
+        new.sort()
+        return print(new)
