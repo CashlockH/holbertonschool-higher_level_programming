505a1d8b9a9986a814f81ba7b3a36add9068d36b
CashlockH <ilyashesenli48@gmail.com>
2024-06-02 02:04:10 +0400

    class documentation

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -5,6 +5,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """subclass of Rectangle class"""
     def __init__(self, size):
         self.__size = size
         super().integer_validator("size", self.__size)
