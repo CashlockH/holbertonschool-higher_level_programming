573e6e78855797cfbcc87a2e3ef598683fc889fb
CashlockH <ilyashesenli48@gmail.com>
2024-06-02 02:09:41 +0400

    str method adapted for Square class

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module for Square class"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """subclass of Rectangle class"""
+    def __init__(self, size):
+        self.__size = size
+        super().integer_validator("size", self.__size)
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
