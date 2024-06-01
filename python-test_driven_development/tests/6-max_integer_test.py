82053c0958f4cb32baaa6525be40b66dec136430
CashlockH <ilyashesenli48@gmail.com>
2024-06-01 14:05:22 +0400

    all test cases & pycodestyle

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -4,10 +4,17 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-class TestMethods(unittest.TestCase):
 
+class TestMethods(unittest.TestCase):
     def test_regular(self):
-       self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([4, 2, 3, 1]), 4)
+        self.assertEqual(max_integer([1, 2, 4, 3]), 4)
+        self.assertEqual(max_integer([1, -1, 4, 3]), 4)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([]), None)
+
 
 if __name__ == '__main__':
     unittest.main()
