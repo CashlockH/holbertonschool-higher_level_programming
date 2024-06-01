ed2413e2da772772869f599572a3821fd32e13a8
CashlockH <ilyashesenli48@gmail.com>
2024-06-01 14:32:51 +0400

    condition for perimeter

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -12,6 +12,8 @@ class Rectangle():
         return self.width * self.height
 
     def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
         return 2 * (self.width + self.height)
 
     @property
