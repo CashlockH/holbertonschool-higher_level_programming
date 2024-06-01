52df2e866b7e105d328c3b2a6db7df7c7d34c59f
CashlockH <ilyashesenli48@gmail.com>
2024-06-01 14:48:25 +0400

    condition for str method and pycodestyle

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -7,22 +7,26 @@ class Rectangle():
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+
     def __str__(self):
         string = ""
+        if self.height == 0 or self.width == 0:
+            return string
         for i in range(self.height):
             for j in range(self.width):
                 string = string + '#'
             if i < self.height - 1:
                 string = string + '\n'
         return string
+
     def area(self):
         return self.width * self.height
-        
+
     def perimeter(self):
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.width + self.height)
-    
+
     @property
     def width(self):
         return self.__width
@@ -30,7 +34,7 @@ class Rectangle():
     @property
     def height(self):
         return self.__height
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
