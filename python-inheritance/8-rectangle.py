d00ca466e2566bc0d65aa690d18e39f6e3c31b97
CashlockH <ilyashesenli48@gmail.com>
2024-06-02 00:41:40 +0400

    subclass for BaseGeometry

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""module for BaseGeometry class"""
+
+
+class BaseGeometry:
+    """class with area and validator methods"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        self.name = name
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(self.name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(self.name))
+        self.value = value
+
+
+class Rectangle(BaseGeometry):
+    """subclass of BaseGeometry"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("Rectangule", self.__width)
+        self.integer_validator("Rectangule", self.__height)
