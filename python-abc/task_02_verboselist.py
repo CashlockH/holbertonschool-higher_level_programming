9ea184957681732708bb39e2e97f2054b1eb934b
CashlockH <ilyashesenli48@gmail.com>
2024-06-02 15:06:19 +0400

    class inherits from list

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Abstract classes"""
+
+
+class VerboseList(list):
+    """Extends list class"""
+    def append(self, item):
+        super().append(item)
+        print("Added {} to the list.".format(item))
+
+    def extend(self, items):
+        super().extend(items)
+        print("Extended the list with {} items".format(len(items)))
+
+    def remove(self, item):
+        print("Removed {} from the list.".format(item))
+        super().remove(item)
+
+    def pop(self, item=-1):
+        print("Popped {} from the list.".format(self[item]))
+        super().pop(item)
