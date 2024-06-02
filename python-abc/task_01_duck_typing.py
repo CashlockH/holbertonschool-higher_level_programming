e278b13c7beb267dd68e5a24ada038fe94d814a1
CashlockH <ilyashesenli48@gmail.com>
2024-06-02 14:37:39 +0400

    duplication removed

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -3,7 +3,6 @@
 from abc import ABC, abstractmethod
 from math import pi
 
-
 class Shape(ABC):
     """Abstract class with area and perimeter methods"""
     @abstractmethod
@@ -13,53 +12,12 @@ class Shape(ABC):
     @abstractmethod
     def perimeter():
         pass
-
 
 class Circle(Shape):
     """Circle class inherits from Shape"""
     def __init__(self, radius):
         if radius < 0:
             radius = radius * -1
-        self.radius = radius
-
-    def area(self):
-        return (self.radius ** 2) * pi
-
-    def perimeter(self):
-        return 2 * self.radius * pi
-
-
-class Rectangle(Shape):
-    """Rectangle class inherits from Shape"""
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-    def area(self):
-        return self.width * self.height
-
-    def perimeter(self):
-        return 2 * (self.width + self.height)
-
-
-#!/usr/bin/env python3
-"""Abstract classes"""
-from abc import ABC, abstractmethod
-from math import pi
-
-class Shape(ABC):
-    """Abstract class with area and perimeter methods"""
-    @abstractmethod
-    def area():
-        pass
-
-    @abstractmethod
-    def perimeter():
-        pass
-
-class Circle(Shape):
-    """Circle class inherits from Shape"""
-    def __init__(self, radius):
         self.radius = radius
 
     def area(self):
@@ -81,5 +39,5 @@ class Rectangle(Shape):
         return 2 * (self.width + self.height)
 
 def shape_info(duck):
-    print(duck.area())
-    print(duck.perimeter())
+    print("Area: {}".format(duck.area()))
+    print("Perimeter: {}".format(duck.perimeter()))
