c0e03eefb74a4f191df70248f7ab8b74de38549f
CashlockH <ilyashesenli48@gmail.com>
2024-06-02 15:08:23 +0400

    brackets added to the string

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -6,16 +6,16 @@ class VerboseList(list):
     """Extends list class"""
     def append(self, item):
         super().append(item)
-        print("Added {} to the list.".format(item))
+        print("Added [{}] to the list.".format(item))
 
     def extend(self, items):
         super().extend(items)
-        print("Extended the list with {} items".format(len(items)))
+        print("Extended the list with [{}] items".format(len(items)))
 
     def remove(self, item):
-        print("Removed {} from the list.".format(item))
+        print("Removed [{}] from the list.".format(item))
         super().remove(item)
 
     def pop(self, item=-1):
-        print("Popped {} from the list.".format(self[item]))
+        print("Popped [{]} from the list.".format(self[item]))
         super().pop(item)
