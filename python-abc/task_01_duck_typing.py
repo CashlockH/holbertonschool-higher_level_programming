dc8256ef414301799b710a56bd7ec1277a4e5f69
CashlockH <ilyashesenli48@gmail.com>
2024-06-02 12:39:18 +0400

    output correction

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -42,6 +42,44 @@ class Rectangle(Shape):
         return 2 * (self.width + self.height)
 
 
+#!/usr/bin/env python3
+"""Abstract classes"""
+from abc import ABC, abstractmethod
+from math import pi
+
+class Shape(ABC):
+    """Abstract class with area and perimeter methods"""
+    @abstractmethod
+    def area():
+        pass
+
+    @abstractmethod
+    def perimeter():
+        pass
+
+class Circle(Shape):
+    """Circle class inherits from Shape"""
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return (self.radius ** 2) * pi
+
+    def perimeter(self):
+        return 2 * self.radius * pi
+
+class Rectangle(Shape):
+    """Rectangle class inherits from Shape"""
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
 def shape_info(duck):
     print(duck.area())
     print(duck.perimeter())
