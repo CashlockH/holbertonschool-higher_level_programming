b09584cddf9ea899e1d3e61aee1a68efbf264356
CashlockH <ilyashesenli48@gmail.com>
2024-06-05 18:48:50 +0400

    attrs in class

---
+++ python-input_output/10-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Student class"""
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs != None:
+            new = dict()
+            for i in attrs:
+                if i in self.__dict__:
+                    new[i] = self.__dict__[i]
+            return new
+        return self.__dict__
