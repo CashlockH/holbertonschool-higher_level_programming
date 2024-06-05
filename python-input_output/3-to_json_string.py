5edb644b00f323ead5b5d3cbe1b0174c3866b599
CashlockH <ilyashesenli48@gmail.com>
2024-06-05 15:15:45 +0400

    json represantation

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""to_json_string function module"""
+import json
+
+
+def to_json_string(my_obj):
+    """returns json representation of my_obj"""
+    return json.dumps(my_obj)
