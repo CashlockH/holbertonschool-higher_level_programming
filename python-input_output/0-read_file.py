25775e1ecb161b59303888297d611b7970ad430f
CashlockH <ilyashesenli48@gmail.com>
2024-06-05 14:52:29 +0400

    documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Holds read_file function"""
+
+
 def read_file(filename=""):
+    """Opens the file and read from that"""
     with open(filename, encoding="utf-8") as file:
         file.read()
