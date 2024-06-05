2eebf5b778ce722af65d0f6dbdc0deb31014ed93
CashlockH <ilyashesenli48@gmail.com>
2024-06-05 15:00:17 +0400

    read text is printed to screen

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=""):
     """Opens the file and read from that"""
     with open(filename, encoding="utf-8") as file:
-        file.read()
+        a = file.read()
+        print(a, end="")
