c198d196529d94ee0de840e26f2035ad0040e2b3
CashlockH <ilyashesenli48@gmail.com>
2024-06-05 15:06:39 +0400

    return value

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """Opens a file and write a text into it"""
     with open(filename, 'w', encoding="utf-8") as file:
-        file.write(text)
+        return file.write(text)
