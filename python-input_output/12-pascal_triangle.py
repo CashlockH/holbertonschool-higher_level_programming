c80570b58d0eac58a8611c022612de12f709f0fe
CashlockH <ilyashesenli48@gmail.com>
2024-06-06 19:08:31 +0400

    algorithm changed completely

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -5,16 +5,14 @@
 def pascal_triangle(n):
     """prints n row of pascal triangle"""
     triangle = []
-    a = 1
     for i in range(n):
         j = 0
         sub_tri = []
         while j <= i:
-            if j - 1 < i - j and j != 0:
-                a += i - j
-            elif j - 1 > i - j:
-                a = a - j + 1
-            sub_tri.append(a)
+            if i != 0 and j != 0 and j != i:
+                sub_tri.append(triangle[i - 1][j] + triangle[i - 1][j - 1])
+            else:
+                sub_tri.append(1)
             j += 1
         triangle.append(sub_tri)
     return triangle
