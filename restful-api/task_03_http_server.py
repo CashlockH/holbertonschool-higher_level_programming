2dccbadbfc5224d0169f5f2e5908f36bdd56c231
CashlockH <ilyashesenli48@gmail.com>
2024-06-09 10:14:26 +0400

    basic http request handler

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,36 @@
+"""HTTP Handler"""
+import http.server
+import socketserver
+import json
+
+
+class Site(http.server.BaseHTTPRequestHandler):
+    """Basic Site Class"""
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a simple API!')
+        elif self.path == '/data':
+            a = json.dumps({"name": "John", "age": 30, "city": "New York"})
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(a.encode('utf-8'))
+        elif self.path == '/info':
+            a = json.dumps({
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+                })
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(a.encode('utf-8'))
+        else:
+            self.send_response(404, message="Endpoint not found")
+
+    def serv_forever(Port):
+        with socketserver.TCPServer(("", Port), Site) as httpd:
+            print("serving at port", Port)
+            httpd.serve_forever()
