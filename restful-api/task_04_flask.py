3625c3e2c3cc5c7045cf8135559f055f30ede6e6
CashlockH <ilyashesenli48@gmail.com>
2024-06-09 13:14:08 +0400

    adding user without username

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -31,22 +31,25 @@ def user(username):
 
 @app.route('/add_user', methods=['GET', 'POST'])
 def parser():
-    usera = request.json
-    if not usera:
-        return jsonify({"error": "No JSON data provided"}), 400
-    if 'username' not in usera:
-        return jsonify({"error": "Username is required"}), 400
-    users[usera['username']] = usera
-    new = {
-        "message": "User added",
-        "user": {
-            "username": usera['username'],
-            "name": usera['name'],
-            "age": usera['age'],
-            "city": usera['city']
-            }
-        }
-    return jsonify(new), 200
+    try:
+        usera = request.json
+        if not usera:
+            return jsonify({"error": "No JSON data provided"}), 400
+        if 'username' not in usera:
+            return jsonify({"error": "Username is required"}), 400
+        users[usera['username']] = usera
+        new = {
+                "message": "User added",
+                "user": {
+                    "username": usera['username'],
+                    "name": usera['name'],
+                    "age": usera['age'],
+                    "city": usera['city']
+                    }
+                }
+        return jsonify(new), 201
+    except Exception as err:
+        return jsonify({"error": str(err)}), 404
 
 
 if __name__ == "__main__":
