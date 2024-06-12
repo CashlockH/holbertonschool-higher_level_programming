b149bb385dc89a890571f219ae52a289833d1798
CashlockH <ilyashesenli48@gmail.com>
2024-06-12 23:10:20 +0400

    Basic Auth

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,42 +1,27 @@
 """Authenticated Flask website"""
+from flask import Flask
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask import Flask
-from flask import jsonify
-from flask import request
-
-from flask_jwt_extended import create_access_token
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
-from flask_jwt_extended import JWTManager
-
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
 users = {
-      "user1": {
-          "username": "user1",
-          "password": generate_password_hash("belede"),
-          "role": "user"
-          },
-      "admin1": {
-          "username": "admin1",
-          "password": generate_password_hash("adminemde"),
-          "role": "admin"}
-  }
+    "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
+    "admin1": {"username": "admin1", "password": generate_password_hash("admin"), "role": "admin"}
+}
 
 @auth.verify_password
 def verify_password(username, password):
     if username in users and check_password_hash(users[username]['password'], password):
         return username
+    return None
 
 
-@app.route('/basic-protected')
+@app.route('/basic-protected', methods = ['GET'])
 @auth.login_required
-def index():
+def protected():
     return "Basic Auth: Access Granted"
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
