173abed7fe21170d8955f2f1e8a299e24bf54245
CashlockH <ilyashesenli48@gmail.com>
2024-06-13 09:45:41 +0400

    code correction

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -55,28 +55,10 @@ def jwt_protected():
 def admin_only():
     current_user = get_jwt_identity()
     if current_user['role'] == 'admin':
-        return "Admin Access: Granted", 200
-    return jsonify({"error": "Admins only"}), 403
+        return current_user
+    return jsonify({"error": "Only admin"}), 403
 
-@jwt.unauthorized_loader
-def handle_unauthorized_error(err):
-    return jsonify({"error": "Missing or invalid token"}), 401
 
-@jwt.invalid_token_loader
-def handle_invalid_token_error(err):
-    return jsonify({"error": "Invalid token"}), 401
-
-@jwt.expired_token_loader
-def handle_expired_token_error(err):
-    return jsonify({"error": "Token has expired"}), 401
-
-@jwt.revoked_token_loader
-def handle_revoked_token_error(err):
-    return jsonify({"error": "Token has been revoked"}), 401
-
-@jwt.needs_fresh_token_loader
-def handle_needs_fresh_token_error(err):
-    return jsonify({"error": "Fresh token required"}), 401
 
 if __name__ == '__main__':
     app.run()
