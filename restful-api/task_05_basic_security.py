bdceb90eeadc22ab04c55668f891f6ce5fe6d346
CashlockH <ilyashesenli48@gmail.com>
2024-06-13 10:21:15 +0400

    error handling

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -48,19 +48,35 @@ def login():
 @jwt_required()
 def jwt_protected():
     current_user = get_jwt_identity()
-    if current_user['username'] in users:
-        return "JWT Auth: Access Granted", 200
-    return jsonify({"error": "Invalid token"}), 401
+    return "JWT Auth: Access Granted", 200
 
 @app.route('/admin-only', methods=["GET"])
 @jwt_required()
 def admin_only():
     current_user = get_jwt_identity()
     if current_user['role'] == 'admin' and current_user['username'] in users:
-        return current_user
-    return jsonify({"error": "Only admin"}), 403
+        return "Admin Access: Granted"
+    return jsonify({"error": "Forbidden"}), 403
 
+@jwt.unauthorized_loader
+def handle_unauthorized_error(err):
+    return jsonify({"error": "Missing or invalid token"}), 401
 
+@jwt.invalid_token_loader
+def handle_invalid_token_error(err):
+    return jsonify({"error": "Invalid token"}), 401
+
+@jwt.expired_token_loader
+def handle_expired_token_error(err):
+    return jsonify({"error": "Token has expired"}), 401
+
+@jwt.revoked_token_loader
+def handle_revoked_token_error(err):
+    return jsonify({"error": "Token has been revoked"}), 401
+
+@jwt.needs_fresh_token_loader
+def handle_needs_fresh_token_error(err):
+    return jsonify({"error": "Fresh token required"}), 401
 
 if __name__ == '__main__':
     app.run()
