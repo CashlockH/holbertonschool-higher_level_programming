cfee6e9ff06d58a6f899e6855b3e072559023d6c
CashlockH <ilyashesenli48@gmail.com>
2024-06-30 23:30:13 +0400

    7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,17 @@
+"""Module"""
+
+from model_state import Base, State
+import SQLAlchemy
+from sqlalchemy import create_engine
+import sys
+from sqlalchemy.orm import sessionmaker
+if __name__ == '__main__':
+    u = sys[1]
+    p = sys[2]
+    d_n = sys[3]
+    engine = create_engine(f'mysql+mysqldb://{u}:{p}@localhost/{d_n}')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print(state)
