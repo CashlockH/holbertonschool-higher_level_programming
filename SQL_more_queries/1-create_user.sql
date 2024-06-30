c8254e09e6cca5ca6d4dfd290c8b3b49a4f7a6e9
CashlockH <ilyashesenli48@gmail.com>
2024-06-30 18:08:42 +0400

    typo

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates user if not existS
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH 'user_0d_1_pwd';
-GRANT ALL PRIVILIGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
