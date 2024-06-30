b3a9f4713f82f446079a893403818d9a3bb9d8f9
CashlockH <ilyashesenli48@gmail.com>
2024-06-30 20:00:26 +0400

    script that lists all shows contained in hbtn_0d_tvshows without a genre linked.

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -4,11 +4,11 @@ SELECT
   tv_show_genres.genre_id
 FROM
   tv_shows
-JOIN
+LEFT JOIN
   tv_show_genres
 ON
   tv_shows.id = tv_show_genres.show_id
-WHERE table2.col2 IS NULL
+WHERE tv_show_genres.genre_id IS NULL
 ORDER BY
-  tv_shows.title ASC,
-  tv_show_genres.genre_id ASC;
+  tv_shows.title,
+  tv_show_genres.genre_id;
