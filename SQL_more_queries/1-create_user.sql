0da1fa72ce6e5b1b9217b10e035142268ccae78a
CashlockH <ilyashesenli48@gmail.com>
2024-06-30 18:16:11 +0400

    comment

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- creates user if not existS
 CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+-- all privileges to the user
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%' WITH GRANT OPTION;
