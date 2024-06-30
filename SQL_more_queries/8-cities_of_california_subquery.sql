c543ffd09ecb63c80088f6ece7d33d98831e05a2
CashlockH <ilyashesenli48@gmail.com>
2024-06-30 18:59:46 +0400

    style

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,17 @@
 -- cities of California
-SELECT id, name FROM cities
-WHERE state_id = (
-	SELECT id FROM state
-	WHERE name = "California")
-ORDER BY id ASC;
+SELECT
+  id,
+  name
+FROM
+  cities
+WHERE
+  state_id = (
+    SELECT
+      id
+    FROM
+      state
+    WHERE
+      name = "California"
+  )
+ORDER BY
+  id ASC;
