39298655181b85008c2f7750c3ca4e7bb53d1614
CashlockH <ilyashesenli48@gmail.com>
2024-06-30 22:00:06 +0400

    select all states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+username_sys = sys.argv[1]
+password_sys = sys.argv[2]
+database_name = sys.argv[3]
+db=MySQLdb.connect(host="localhost", port=3306, user = username_sys, password=password_sys, database=database_name)
+c = db.cursor()
+c.execute("""SELECT id, name FROM states ORDER BY id""")
+c.fetchall()
+c.close()
