728ae21000c55d5b4369732be828b1bd466358ed
CashlockH <ilyashesenli48@gmail.com>
2024-06-30 22:22:37 +0400

    should not run when imported

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,14 +2,15 @@
 """Mysql database"""
 import MySQLdb
 import sys
-username_sys = sys.argv[1]
-password_sys = sys.argv[2]
-database_name = sys.argv[3]
-db=MySQLdb.connect(host="localhost", port=3306, user = username_sys, password=password_sys, database=database_name)
-c = db.cursor()
-c.execute("""SELECT id, name FROM states ORDER BY id""")
-states = c.fetchall()
-for state in states:
-    print(state)
-c.close()
-db.close()
+if __name__ == '__main__':
+    username_sys = sys.argv[1]
+    password_sys = sys.argv[2]
+    database_name = sys.argv[3]
+    db=MySQLdb.connect(host="localhost", port=3306, user = username_sys, password=password_sys, database=database_name)
+    c = db.cursor()
+    c.execute("""SELECT id, name FROM states ORDER BY id""")
+    states = c.fetchall()
+    for state in states:
+        print(state)
+    c.close()
+    db.close()
