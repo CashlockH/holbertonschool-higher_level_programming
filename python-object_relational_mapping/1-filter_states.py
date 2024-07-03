678c69a985129a723f939e0a42992c131d27e112
CashlockH <ilyashesenli48@gmail.com>
2024-07-03 09:27:50 +0000

    condition correction

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -2,16 +2,16 @@
 """Filtering state query"""
 import MySQLdb
 import sys
-if __name__ = "__main__":
+if __name__ == "__main__":
     username = sys.argv[1]
     password_sys = sys.argv[2]
     database_name = sys.argv[3]
     db = MySQLdb.connect(
         host="localhost",
-        port = 3306,
-        user = username,
-        password = password_sys,
-        database = database_name
+        port=3306,
+        user=username,
+        password=password_sys,
+        database=database_name
         )
     c = db.cursor()
     c.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id""")
@@ -19,3 +19,4 @@ if __name__ = "__main__":
     for state in states:
         print(state)
     c.close()
+    db.close()
