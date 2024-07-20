7773c600fc86d15d89b6c57de66a2d4cc058239d
CashlockH <ilyashesenli48@gmail.com>
2024-07-20 21:49:07 +0400

    not know

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import json
 import csv
+
 app = Flask(__name__)
 
 @app.route('/products')
@@ -9,21 +10,20 @@ def search():
     id = request.args.get('id')
     data = file_handler(source)
     if data == 0:
-        return
-    return render_template('product_display.html', data = data, id = int(id) if id else id), 200
+        return "Invalid source", 400
+    return render_template('product_display.html', data=data, id=int(id) if id else id), 200
 
 def file_handler(source):
     if source == "json":
         with open("products.json") as json_file:
-            data = json.loads(json_file)
+            data = json.load(json_file)
         return data
     elif source == "csv":
         list_of_csv = []
         with open('products.csv', newline='') as csvfile:
-            reader = csv.reader(csvfile)
+            reader = csv.DictReader(csvfile)
             for row in reader:
-                if not row[0] == "id":
-                    list_of_csv.append({'id': row[0], 'name': row[1], 'category': row[2], 'price': row[3]})
+                list_of_csv.append({'id': row['id'], 'name': row['name'], 'category': row['category'], 'price': row['price']})
         return list_of_csv
     else:
         return 0
