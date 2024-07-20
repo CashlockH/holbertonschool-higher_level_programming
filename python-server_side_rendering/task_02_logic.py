465fe343b2e1054013eea1c032c449aa81713adf
CashlockH <ilyashesenli48@gmail.com>
2024-07-20 22:14:04 +0400

    %if%

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -6,8 +6,6 @@ app = Flask(__name__)
 def home():
     with open('items.json') as file:
         data = json.load(file)
-    if not data:
-        return "No items found", 200
     return render_template('items.html', items = data["items"])
 
 if __name__ == '__main__':
