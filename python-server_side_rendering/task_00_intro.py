ef0e74996e446fb7203d56f65b465d236c911475
CashlockH <ilyashesenli48@gmail.com>
2024-07-20 16:56:57 +0400

    s

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -10,6 +10,6 @@ def generate_invitations(template, attendees):
         return
     for index, element in enumerate(attendees):
         file = "output_{}.txt".format(index)
-        if os.path.exist(file) is False:
+        if os.path.exists(file) is False:
             with open(file, 'w') as f:
                 f.write(template.format(name = element["name"] if element["name"] else "name: N/A", event_title = element["event_title"] if element["event_title"] else "event_title: N/A", event_date = element["event_date"] if element["event_date"] else "event_date: N/A", event_location = element["event_location"] if element["event_location"] else "event_location: N/A"))
