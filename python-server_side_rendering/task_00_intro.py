51013ee6f4e6ca499776f62b1f037136237ef28d
CashlockH <ilyashesenli48@gmail.com>
2024-07-20 17:17:10 +0400

    missing date

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -12,4 +12,15 @@ def generate_invitations(template, attendees):
         file = "output_{}.txt".format(index)
         if os.path.exists(file) is False:
             with open(file, 'w') as f:
-                f.write(template.format(name = element["name"] if element["name"] else "name: N/A", event_title = element["event_title"] if element["event_title"] else "event_title: N/A", event_date = element["event_date"] if element["event_date"] else "event_date: N/A", event_location = element["event_location"] if element["event_location"] else "event_location: N/A"))
+                f.write(template.format(name = checker("name", element),
+                                        event_title = checker("event_title", element),
+                                        event_date = checker("event_date", element),
+                                        event_location = checker("event_location", element)))
+
+def checker(key, dict):
+    if key not in dict:
+        return "{}: N/A".format(key)
+    elif not dict[key]:
+        return "N/A"
+    else:
+        return "{}".format(dict[key])
